8b370038be619295d2878fd3521d410b30bfc488
Martí Soler <info@msc-soft.com>
2026-02-18 21:12:29 +0100

    Reorde the projects

---
+++ NetCoreFileAccess.UnitTest/PasswordPatternTest.cs
@@ -0,0 +1,30 @@
+﻿using NUnit.Framework;
+using NetCoreFileAccess;
+
+namespace SecureBunker.UnitTests
+{
+    [TestFixture]
+    public class PasswordPatternTest
+    {
+        [SetUp]
+        public void Setup()
+        {
+            // Any necessary setup before each test
+            CredentialsUtils.Pattern = @"^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{4,20}$";
+        }
+
+        [Test]
+        [TestCase("aA1!", true)] //Pass all conditions
+        [TestCase("aA1", false)] //Fail, less than minimum characters long
+        [TestCase("AaDdFt", false)]  //Fail, without number
+        [TestCase("1234596", false)] //Fail, without letter
+        [TestCase("Aa125", false)] //Fail, without special character
+        [TestCase("1234596AaAhskdfgnkdasdlkjl", false)] //Fail, too long
+        public void CheckPassword_CorrectPassword_Pass(string Password, bool pass)
+        {            
+            bool result = CredentialsUtils.CheckPassword(Password);
+            Assert.That(result, Is.EqualTo(pass));
+        }
+
+    }
+}
